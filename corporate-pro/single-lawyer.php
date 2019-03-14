c773ab599c6e48e4a9c670f49b185d75d141fa36
Nick Young <mycorpweb@gmail.com>
2019-03-14 14:54:45 -0600

    Remove footer area from lawyer template

--- corporate-pro/single-lawyer.php
+++ corporate-pro/single-lawyer.php
@@ -5,25 +5,12 @@
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
 function iwd_single_lawyer() {
-	//the_content();
 
 	$params = array(
 		'limit'   => -1  // Return all rows
 	);
 
 	$pods = pods( 'lawyer', $params );
-
-	/*
-
-	if ( $pods->field( 'speaking-engagements' ) ) {
-		echo '<strong>Speaking Engagements</strong>';
-		echo $pods->display( 'speaking-engagements', true );
-	}
-
-	if ( $pods->field( 'publications' ) ) {
-		echo '<strong>Publications</strong>';
-		echo $pods->display( 'publications', true );
-	}*/
 
 	?>
 		<div class="walaw-single-lawyer">
@@ -37,22 +24,32 @@ function iwd_single_lawyer() {
 						<strong>Contact</strong>
 						<?php
 							if ( $pods->field( 'contact-phone' ) ) {
-								echo '<p>' . $pods->display( 'contact-phone', true ) . '</p>';
+								echo '<p>Phone: ' . $pods->display( 'contact-phone', true ) . '</p>';
 							}
 
 							if ( $pods->field( 'contact-email' ) ) {
-								echo '<p>' . $pods->display( 'contact-email', true ) . '</p>';
+								echo '<p>Email: <a href="mailto:' . esc_attr( $pods->display( 'contact-email', true ) ) . '">' . $pods->display( 'contact-email', true ) . '</a></p>';
 							}
 						?>
 					</div>
 				<?php } ?>
 
 				<?php if ( $pods->field( 'practice-areas' ) ) { ?>
+
+					<?php
+
+					$practice_area = $pods->fetch( 'practice-areas' );
+
+					if ( ! empty( $practice_area['practice-areas'] ) ) {
+					?>
 					<div>
 						<strong>Practice Areas</strong>
-						<?php echo $pods->display( 'practice-areas', true ); ?>
+						<p>
+							<?php echo '<a href="' . get_category_link( $practice_area['practice-areas']['term_id'] ) .'">' . $practice_area['practice-areas']['name'] . '</a>'; ?>
+						</p>
+
 					</div>
-				<?php } ?>
+				<?php } } ?>
 
 				<?php if ( $pods->field( 'education' ) ) { ?>
 					<div>
@@ -77,8 +74,8 @@ remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
 remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
 add_action( 'genesis_entry_content', 'iwd_single_lawyer' );
 
-//remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
-//remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
+remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
+remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
 
 
 function iwd_remove_post_meta($post_meta) {
@@ -87,7 +84,7 @@ function iwd_remove_post_meta($post_meta) {
 		return $post_meta;
 	}*/
 
-	//return '';
+	return '';
 }
 add_filter( 'genesis_post_meta', 'iwd_remove_post_meta' );
 
